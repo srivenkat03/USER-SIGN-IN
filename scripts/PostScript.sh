6b78da9b877180f7f1d42d7eedc3145fe072ea11
V Sri Venkat <v.srivenkat@qsc.com>
2025-08-19 22:29:11 +0530

    updated the appspec

--- scripts/PostScript.sh
+++ scripts/PostScript.sh
@@ -2,10 +2,6 @@
 # Pull the image and run detached on port 5050:5050
 
 # Check if container exists
-if [ "$(docker ps -a -q -f name=user-sign-in-app)" ]; then
-  echo "Restarting existing container user-sign-in-app..."
-  docker restart user-sign-in-app
-else
-  docker pull srivenkat03/user-sign
-  docker run --name user-sign-in-app -d -p 5050:5050 srivenkat03/user-sign
-fi
+docker pull srivenkat03/user-sign
+docker run --name user-sign-in-app -d -p 5050:5050 srivenkat03/user-sign
+
--- scripts/PreScript.sh
+++ scripts/PreScript.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Stop and remove all running containers if any exist
-if [ "$(docker ps -q)" ]; then
-  docker stop $(docker ps -q)
-  docker rm $(docker ps -q)
+if [ "$(docker ps -a -q -f name=user-sign-in-app)" ]; then
+  echo "Stopping existing container user-sign-in-app..."
+  docker stop user-sign-in-app
 fi
