82b5df48ab12a737e8523fc91a19127f0271f0da
V Sri Venkat <v.srivenkat@qsc.com>
2025-08-19 18:05:38 +0530

    updated the post script

--- scripts/PostScript.sh
+++ scripts/PostScript.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Pull the image and run detached on port 5050:5050
 
-docker pull srivenkat03/user-sign
-
-docker run -d -p 5050:5050 srivenkat03/user-sign
+# Check if container exists
+if [ "$(docker ps -a -q -f name=user-sign-in-app)" ]; then
+  echo "Restarting existing container user-sign-in-app..."
+  docker restart user-sign-in-app
+else
+  docker pull srivenkat03/user-sign
+  docker run --name user-sign-in-app -d -p 5050:5050 srivenkat03/user-sign
+fi
