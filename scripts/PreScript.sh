e3e4db650da49e57f0c24aefdcbc6733cc81e3d5
V Sri Venkat <v.srivenkat@qsc.com>
2025-08-20 22:22:33 +0530

    updated 1

--- scripts/PreScript.sh
+++ scripts/PreScript.sh
@@ -3,4 +3,11 @@
 if [ "$(docker ps -a -q -f name=user-sign-in-app)" ]; then
   echo "Stopping existing container user-sign-in-app..."
   docker stop user-sign-in-app
+  docker rm -f user-sign-in-app
+fi
+
+# Remove the image if it exists
+if [ "$(docker images -q srivenkat03/user-sign)" ]; then
+  echo "Removing image srivenkat03/user-sign..."
+  docker rmi -f srivenkat03/user-sign
 fi
